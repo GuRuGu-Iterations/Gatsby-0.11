961db850e2adff4e43c42bf5865479842ef99164
GuRuGu <peter.k.nets@gmail.com>
2020-01-15 15:55:47 +0200

    Set up basic blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,17 +7,17 @@ module.exports = {
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    title: `Peter's Blog`,
+    author: `Peter Krevenets`,
+    description: `My thoughts about life and everything else...`,
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        url: `https://twitter.com/peter_krevenets`,
       },
       {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/GuRuGuMaWaRu`,
       },
     ],
   },
